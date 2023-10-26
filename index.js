b49d724f4d1bb5fa1f47f3a064b146484d2a9649
annvrk01 <annvrk01@gmail.com>
2023-10-27 00:37:35 +0700

    Session 2 - get data  form NodeJS

--- index.js
+++ index.js
@@ -9,13 +9,43 @@ const server = http.createServer((request, response) => {
   response.write("<h1> Hello An </h1>");
   const { pathname, query } = url.parse(request.url, true)
 
+  console.log('query', query)
+
+  if (request.method == "POST") {
+    let data = "";
+    request
+      .on("error", (err) => {
+        console.error(err);
+      })
+      .on("data", (chunk) => {
+        // console.log(chunk);
+        data += chunk.toString();
+      })
+      .on("end", () => {
+       
+      });
+  }
+
+  let html = '';
   if (pathname === "/") {
     response.write("<h1>Trang chủ </h1>")
   } else if (pathname === "/products") {
     response.write("<h1> Danh sách Products </h1>")
-  }else if (pathname === "/users") {
+  } else if (pathname === "/users") {
     response.write("<h1> Danh sách Users </h1>")
+
+    html += `
+    <form action="http://localhost:8000/users" method="GET">
+        <input type="text" name="keyword" placeholder="Từ khóa" />
+        <input type="text" name="name" placeholder="Từ khóa" />
+        <button type="submit"> Tìm Kiếm </button>
+        <p> Từ khóa ${query?.keyword}  </p>
+    </form>
+`
   }
+  response.write(html)
+
+
   response.end();
 })
 
