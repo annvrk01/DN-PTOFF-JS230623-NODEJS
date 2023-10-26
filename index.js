3eaeae77068d8c0dd40c574b649b22ce4e2acc76
annvrk01 <annvrk01@gmail.com>
2023-10-27 01:09:16 +0700

    Session03 - Đọc ghi file và sử dụng html template

--- index.js
+++ index.js
@@ -1,55 +1,60 @@
 const http = require('http')
 const url = require('url')
+const fs = require("fs")
+const path = require("path")
 
-// khởi tạo 1 máy chủ HTTp
+
+const data = fs.readFileSync("content.txt", "utf-8")
+
+// hướng dẫn new 1 file log
+const action_logs = [
+  {
+    action_name: "login",
+    action_value: "",
+    user_id: 0,
+    created_time_stamp: new Date(),
+  }
+];
+let now = new Date();
+let nowAsString = now.toISOString().split('T')[0];
+let nameFile = "log-" + nowAsString;
+fs.writeFileSync(nameFile + ".txt", JSON.stringify(action_logs));
+
+
 const server = http.createServer((request, response) => {
-
-  // Set response status , type
-  response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-  response.write("<h1> Hello An </h1>");
   const { pathname, query } = url.parse(request.url, true)
+  const extName = path.extname(pathname);
+  let contentType = null;
 
-  console.log('query', query)
-
-  if (request.method == "POST") {
-    let data = "";
-    request
-      .on("error", (err) => {
-        console.error(err);
-      })
-      .on("data", (chunk) => {
-        // console.log(chunk);
-        data += chunk.toString();
-      })
-      .on("end", () => {
-       
-      });
+  switch (extName) {
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
   }
 
-  let html = '';
-  if (pathname === "/") {
-    response.write("<h1>Trang chủ </h1>")
-  } else if (pathname === "/products") {
-    response.write("<h1> Danh sách Products </h1>")
-  } else if (pathname === "/users") {
-    response.write("<h1> Danh sách Users </h1>")
-
-    html += `
-    <form action="http://localhost:8000/users" method="GET">
-        <input type="text" name="keyword" placeholder="Từ khóa" />
-        <input type="text" name="name" placeholder="Từ khóa" />
-        <button type="submit"> Tìm Kiếm </button>
-        <p> Từ khóa ${query?.keyword}  </p>
-    </form>
-`
-  }
-  response.write(html)
-
-
-  response.end();
+  response.writeHead(200, { 'Content-Type': contentType + '; charset=utf-8' })
+  fs.readFile("./" + pathname, (error, data) => {
+    if (error) {
+      console.log('error ', pathname)
+    } else {
+      response.write(data)
+    }
+    response.end();
+  })
 })
 
-// khởi động server port 8000
 server.listen(8000, '127.0.0.1', () => {
-  console.log("Start server  http://localhost:8000 ")
+  console.log("Start server  127.0.0.1:8000 ")
 })
